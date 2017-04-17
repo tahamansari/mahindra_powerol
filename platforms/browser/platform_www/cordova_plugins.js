4e282321aa65081bc59255724a79a3daa6e42c13
Taha Ansari <taha.kreaserv@gmail.com>
2017-04-17 14:42:19 +0530

    added google map

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -15,6 +15,14 @@ module.exports = [
         "clobbers": [
             "cordova.plugins.FileOpener"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-googlemaps/www/googlemaps-cdv-plugin.js",
+        "id": "cordova-plugin-googlemaps.cordova-plugin-googlemaps",
+        "pluginId": "cordova-plugin-googlemaps",
+        "clobbers": [
+            "cordova-plugin-googlemaps"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -23,7 +31,8 @@ module.exports.metadata =
     "cordova-plugin-whitelist": "1.2.2",
     "cordova-plugin-console": "1.0.5",
     "cordova-plugin-statusbar": "1.0.1",
-    "cordova-plugin-fileopener": "1.0.5"
+    "cordova-plugin-fileopener": "1.0.5",
+    "cordova-plugin-googlemaps": "1.4.0"
 }
 // BOTTOM OF METADATA
 });
