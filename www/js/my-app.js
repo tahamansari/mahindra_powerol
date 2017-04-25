f03a2679df84944e4906443ce5fb98f81d21d20d
Taha Ansari <taha.kreaserv@gmail.com>
2017-04-25 15:25:17 +0530

    added video

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -7,11 +7,13 @@ var list_data = {};
 
 var myApp = new Framework7({
 
-    pushState: true,
+    // pushState: true,
     // swipePanel: 'right',
     swipeBackPage: false,
-    preloadPreviousPage: true,
-    uniqueHistory: true,
+
+    preloadPreviousPage: false,
+    uniqueHistory: false,
+
     uniqueHistoryIgnoreGetParameters: true,
     modalTitle: 'Powerol',
     imagesLazyLoadPlaceholder: 'img/lazyload.jpg',
@@ -43,11 +45,14 @@ var mainView = myApp.addView('.view-main', {
 mainView.hideNavbar();
 
 
-// myApp.onPageInit('*', function (page) {
+myApp.onPageInit('*', function (page) {
 
-//   console.log(page.name + ' initialized'); 
+  console.log(page.name + ' initialized'); 
 
-// });
+  $('#map-container').css('visibility','hidden');
+
+
+});
 
 
 myApp.onPageInit('cba', function(page) {
@@ -118,13 +123,17 @@ myApp.onPageInit('cba', function(page) {
 
 });
 
-// myApp.onPageInit('findus_map', function(page) {
+myApp.onPageInit('findus_map', function(page) {
 
-//     $('.backbutton').on('click', function() {
-//         mainView.router.back();
-//     });
 
-// });
+    $('#map-container').css('visibility','visible');
+
+    
+    $('.backbutton').on('click', function() {
+        mainView.router.back();
+    });
+
+});
 
 // myApp.onPageInit('findus_dealer', function(page) {
 //     $('.backbutton').on('click', function() {
@@ -496,8 +505,8 @@ myApp.onPageInit('faq', function(page) {
     }
 });
 
-myApp.onPageInit('home', function(page) {
 
+myApp.onPageInit('home', function(page) {
 
     console.log('home page called');
 
@@ -716,166 +725,9 @@ myApp.onPageInit('index', function(page) {
 
     console.log('index page called');
 
-    var element = document.getElementById("map");
-var mapTypeIds = [];
-for (var type in google.maps.MapTypeId) {
-    mapTypeIds.push(google.maps.MapTypeId[type]);
-}
-
-mapTypeIds.push("OSM");
-mapTypeIds.push("MyGmap");
-mapTypeIds.push("LocalGmap");
-mapTypeIds.push("WebStorageGmap");
-mapTypeIds.push("LocalMyGmap");
-mapTypeIds.push("WebStorageMyGmap");
-
-var map = new google.maps.Map(element, {
-
-    center: new google.maps.LatLng(28.394857, 84.124008),
-    zoom: 8,
-    mapTypeId: "MyGmap",
-    mapTypeControlOptions: {
-        mapTypeIds: mapTypeIds,
-        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
-    }
-});
-
-map.mapTypes.set("OSM", new google.maps.ImageMapType({
-    getTileUrl: getOsmTileImgSrc,
-    tileSize: new google.maps.Size(256, 256),
-    name: "OSM",
-    maxZoom: 15
-}));
-
-map.mapTypes.set("MyGmap", new google.maps.ImageMapType({
-    getTileUrl: getGmapTileImgSrc,
-    tileSize: new google.maps.Size(256, 256),
-    name: "MyGmap",
-    maxZoom: 15
-}));
-
-map.mapTypes.set("LocalGmap", new google.maps.ImageMapType({
-    getTileUrl: getLocalTileImgSrc,
-    tileSize: new google.maps.Size(256, 256),
-    name: "LocalGmap",
-    maxZoom: 15
-}));
-
-map.mapTypes.set("WebStorageGmap", new google.maps.ImageMapType({
-    getTileUrl: getWebStorageTileImgSrc,
-    tileSize: new google.maps.Size(256, 256),
-    name: "WebStorageGmap",
-    maxZoom: 15
-}));
-
-map.mapTypes.set("LocalMyGmap", new google.maps.ImageMapType({
-    getTileUrl: function(coord, zoom) {
-        return checkTileInSprites(coord, zoom) ?
-            getLocalTileImgSrc(coord, zoom) :
-            getGmapTileImgSrc(coord, zoom);
-    },
-    tileSize: new google.maps.Size(256, 256),
-    name: "LocalMyGmap",
-    maxZoom: 15
-}));
-
-map.mapTypes.set("WebStorageMyGmap", new google.maps.ImageMapType({
-    getTileUrl: function(coord, zoom) {
-        var image = getWebStorageTileImgSrc(coord, zoom);
-        return image ? image : getGmapTileImgSrc(coord, zoom);
-    },
-    tileSize: new google.maps.Size(256, 256),
-    name: "WebStorageMyGmap",
-    maxZoom: 15
-}));
 
 
- var locations = [
-  ['Dhaulagiri',28.698465, 83.487350, 3],
-  ['Manaslu', 28.549711, 84.559677, 2],
-  ['Bharatpur', 27.216981,  77.489515, 1],
-  ['Kathmandu', 27.717245,  85.323960, 4],
-
-];
-
-var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 8,
-  center: new google.maps.LatLng(28.394857, 84.124008),
-  mapTypeId: google.maps.MapTypeId.ROADMAP
-});
-
-var infowindow = new google.maps.InfoWindow();
-
-var marker, i;
-
-for (i = 0; i < locations.length; i++) { 
-  marker = new google.maps.Marker({
-    position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-    map: map
-  });
-
-  google.maps.event.addListener(marker, 'click', (function(marker, i) {
-    return function() {
-      infowindow.setContent(locations[i][0]);
-      infowindow.open(map, marker);
-    }
-  })(marker, i));
-}
-
-
-google.maps.event.addListener(map, 'click', function(point) {
-    var marker = new google.maps.Marker({
-        position: point.latLng,
-        map: map
-    });
-    google.maps.event.addListener(marker, 'dblclick', function() {
-        marker.setMap(null);
-    });
-    google.maps.event.addListener(marker, 'click', function() {
-        new google.maps.InfoWindow({
-            content: 'lat: ' + point.latLng.lat() + '<br>lng:' + point.latLng.lng()
-        }).open(map, marker);
-    });
-});
-
-function CustomControl(controlDiv, map, title, handler) {
-    controlDiv.style.padding = '5px';
-
-    var controlUI = document.createElement('DIV');
-    controlUI.style.backgroundColor = 'white';
-    controlUI.style.borderStyle = 'solid';
-    controlUI.style.borderWidth = '2px';
-    controlUI.style.cursor = 'pointer';
-    controlUI.style.textAlign = 'center';
-    controlUI.title = title;
-    controlDiv.appendChild(controlUI);
-
-    var controlText = document.createElement('DIV');
-    controlText.style.fontFamily = 'Arial,sans-serif';
-    controlText.style.fontSize = '12px';
-    controlText.style.paddingLeft = '4px';
-    controlText.style.paddingRight = '4px';
-    controlText.innerHTML = title;
-    controlUI.appendChild(controlText);
-
-    google.maps.event.addDomListener(controlUI, 'click', handler);
-}
-
-var clearWebStorageDiv = document.createElement('DIV');
-
-var clearWebStorageButton = new CustomControl(clearWebStorageDiv, map,
-    '', clearWebStorage);
-
-var prepareWebStorageDiv = document.createElement('DIV');
-
-var prepareWebStorageButton = new CustomControl(prepareWebStorageDiv, map,
-    '', prepareWebStorage);
-
-clearWebStorageDiv.index = 1;
-prepareWebStorageDiv.index = 1;
-map.controls[google.maps.ControlPosition.TOP_RIGHT].push(clearWebStorageDiv);
-map.controls[google.maps.ControlPosition.TOP_RIGHT].push(prepareWebStorageDiv);
-
+    // $('#index_map').html($('#map').html());
 
     // $('.mapoverlay').css('display','none');
 
--- www/js/util.js
+++ www/js/util.js
@@ -15,6 +15,7 @@ function goto_page(page) {
 }
 
 
+
 function validateEmail(email) {
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
@@ -83,7 +84,7 @@ function login() {
 function logout() {
     Lockr.flush();
     token = false;
-    goto_page('login.html');
+    goto_page('index.html');
 }
 
 
@@ -101,6 +102,124 @@ function onDeviceReady() {
     // alert('device ready');
     // $('.mapoverlay').css('display','block');
     // return false;
+
+    var downloaded = Lockr.get('downloaded');
+
+    if(downloaded == undefined){
+
+        Lockr.set('downloaded','true');
+
+        // just download ones agni ppsx
+        var fileTransfer = new FileTransfer();
+        var uri = encodeURI("http://kreaserv-tech.com/mahindra_admin/mahindraPowerol.ppsx");
+        // applicationStorageDirectory
+        fileTransfer.download(
+            uri,
+            cordova.file.externalApplicationStorageDirectory + 'files/download/mahindraPowerol.ppsx',
+            function(entry) {
+                // alert("download complete: " + entry.toURL());
+            },
+            function(error) {
+                alert("download error source " + error.source);
+                alert("download error target " + error.target);
+                alert("download error code" + error.code);
+            },
+            false, {
+                headers: {
+                    "Authorization": "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
+                }
+            }
+        );
+
+        // just download ones agni ppsx
+        var fileTransfer = new FileTransfer();
+        var uri = encodeURI("http://kreaserv-tech.com/mahindra_admin/agni.ppsx");
+        // applicationStorageDirectory
+        fileTransfer.download(
+            uri,
+            cordova.file.externalApplicationStorageDirectory + 'files/download/agni.ppsx',
+            function(entry) {
+                // alert("download complete: " + entry.toURL());
+            },
+            function(error) {
+                alert("download error source " + error.source);
+                alert("download error target " + error.target);
+                alert("download error code" + error.code);
+            },
+            false, {
+                headers: {
+                    "Authorization": "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
+                }
+            }
+        );
+
+        // just download ones pdf1
+        var fileTransfer = new FileTransfer();
+        var uri = encodeURI("http://kreaserv-tech.com/mahindra_admin/NEPAL-BANGLADESH-BROCHURE.pdf");
+        // applicationStorageDirectory
+        fileTransfer.download(
+            uri,
+            cordova.file.externalApplicationStorageDirectory + 'files/download/NEPAL-BANGLADESH-BROCHURE.pdf',
+            function(entry) {
+                // alert("download complete: " + entry.toURL());
+            },
+            function(error) {
+                alert("download error source " + error.source);
+                alert("download error target " + error.target);
+                alert("download error code" + error.code);
+            },
+            false, {
+                headers: {
+                    "Authorization": "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
+                }
+            }
+        );
+
+        // just download ones pdf2
+        var fileTransfer = new FileTransfer();
+        var uri = encodeURI("http://kreaserv-tech.com/mahindra_admin/REST-OF-THE-WORLD-BROCHURE.pdf");
+        // applicationStorageDirectory
+        fileTransfer.download(
+            uri,
+            cordova.file.externalApplicationStorageDirectory + 'files/download/REST-OF-THE-WORLD-BROCHURE.pdf',
+            function(entry) {
+                // alert("download complete: " + entry.toURL());
+            },
+            function(error) {
+                alert("download error source " + error.source);
+                alert("download error target " + error.target);
+                alert("download error code" + error.code);
+            },
+            false, {
+                headers: {
+                    "Authorization": "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
+                }
+            }
+        );
+
+        // just download ones load calculator
+        var fileTransfer = new FileTransfer();
+        var uri = encodeURI("http://kreaserv-tech.com/mahindra_admin/loadcalculator.apk");
+        // applicationStorageDirectory
+        fileTransfer.download(
+            uri,
+            cordova.file.externalApplicationStorageDirectory + 'files/download/loadcalculator.apk',
+            function(entry) {
+                // alert("download complete: " + entry.toURL());
+            },
+            function(error) {
+                alert("download error source " + error.source);
+                alert("download error target " + error.target);
+                alert("download error code" + error.code);
+            },
+            false, {
+                headers: {
+                    "Authorization": "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
+                }
+            }
+        );
+
+    }
 
 
 
@@ -128,22 +247,35 @@ function onDeviceReady() {
     }, false);
 
 
-    setTimeout(function(){ 
-        
-        var token = Lockr.get('token');
-        if(token == undefined){
-            console.log('loading login page');
-            mainView.router.load({
-                url: 'login.html'
-            });
-        }else{
-            console.log('loading home page');
-            mainView.router.load({
-                url: 'home.html'
-            });
-        }
-    }, 3000);
+    var token = Lockr.get('token');
+    if(token == undefined){
+        console.log('loading login page');
+        mainView.router.load({
+            url: 'index.html'
+        });
+    }else{
+        console.log('loading home page');
+        mainView.router.load({
+            url: 'home.html'
+        });
+    }
 
+
+
+    // setTimeout(function(){ 
+    //     var token = Lockr.get('token');
+    //     if(token == undefined){
+    //         console.log('loading login page');
+    //         mainView.router.load({
+    //             url: 'login.html'
+    //         });
+    //     }else{
+    //         console.log('loading home page');
+    //         mainView.router.load({
+    //             url: 'home.html'
+    //         });
+    //     }
+    // }, 3000);
 }
 
 
